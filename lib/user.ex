f723572b9c6fbab67b03869688f771330eccb051
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2016-01-18 20:33:30 -0500

    implemented User as struct with a to_string impl

--- lib/user.ex
+++ lib/user.ex
@@ -2,16 +2,24 @@ defmodule Spell.User do
   @doc """
   Starts a user with the given ID.
   """
+  defstruct user_id: nil, name: nil, current_room: nil
 
   def start_link(user_id) do
-    Agent.start_link(fn -> [] end, name: user_id)
+    user = %Spell.User{user_id: user_id, name: "Wompus", current_room: nil}
+    Agent.start_link(fn -> user end, name: user_id)
   end
 
   def get_state(user_id) do
-    Agent.get(user_id, fn list -> list end)
+    Agent.get(user_id, fn user -> user end)
   end
 
-  def set_state(user_id, value) do
-    Agent.update(user_id, fn state -> value end)
+  def set_state(user_id, new_user) do
+    Agent.update(user_id, fn old_user -> new_user end)
   end
+end
+
+defimpl String.Chars, for: Spell.User do
+    def to_string(user) do
+       "User(#{user.user_id})"
+    end
 end
